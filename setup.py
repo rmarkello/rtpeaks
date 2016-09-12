6236364998fe8ff4b3beaa288bcd60a577b0143c
Ross Markello <rdm222@cornell.edu>
2016-09-12 16:30:20 -0400

    Added setup.py and removed interactive test scripts
    Though the setup.py is probably useless since this only works
    on Windows 7.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from rtpeaks.version import __version__
+
+setup(  
+    name='rtpeaks',
+    version=__version__
+    description='Real-time peak detection with BioPac MP150',
+    maintainer='Ross Markello',
+    maintainer_email='rossmarkello@gmail.com',
+    url='http://github.com/rmarkello/rtpeaks',
+    install_requires=['numpy','scipy'],
+    packages=find_packages(exclude=['setup','rtpeaks/tests']),
+    license='GNU3'
+)
